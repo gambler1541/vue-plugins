b2ebf4d6280be6a1d9f0f5b41494f64d69d9c3f4
parkmyengbae <gambler1541@gmail.com>
2022-11-14 12:21:36 +0900

    모듈 플러그인

---
+++ src/components/BarChart.vue
@@ -0,0 +1,46 @@
+<template>
+    <canvas ref="barChart" id="barChart"></canvas>
+</template>
+
+<script>
+import { Chart } from 'chart.js'
+
+export default {
+    mounted() {
+        const labels = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45],
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {}
+        };
+
+        const myChart = new this.$_Chart(
+            // document.getElementById('barChart'),
+            this.$refs.barChart,
+            config
+        );        
+    }
+}
+</script>
+
+<style>
+
+</style>
